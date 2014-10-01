5368cf0d120127517ccc54807dc60af4273fb448
= <=>
2014-10-01 16:21:27 +0530

    adding the bundle and ui package to versio control

---
+++ bundle/src/test/java/com/capitolgroup/cq5/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.capitolgroup.cq5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
